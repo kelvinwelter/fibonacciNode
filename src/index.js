af22f17c3192427be7c73114fec016b04bbf3480
Kelvin Welter <kelvinwelter.b@gmail.com>
2020-04-24 22:07:10 -0300

    minor code improvements

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ function isSquareNumber(num) {
 
 const fibonacci = () => {
     let fibList = [0, 1];
-    while (fibList[fibList.length - 1] <= 350) {
+    const limit = 350;
+    while (fibList[fibList.length - 1] <= limit) {
         const lastElement = fibList[fibList.length - 1];
         const beforeLastElement = fibList[fibList.length - 2];
         fibList.push(lastElement + beforeLastElement);
@@ -15,11 +16,8 @@ const fibonacci = () => {
 }
 
 const isFibonnaci = (num) => {
-    if (isSquareNumber(5*(num*num)-4) || isSquareNumber(5*(num*num)+4)) {
-        return true;
-    } else {
-        return false;
-    }
+    const magicNumber = 5*(num*num); 
+    return (isSquareNumber(magicNumber-4) || isSquareNumber(magicNumber+4));
 }
 
 module.exports = {
